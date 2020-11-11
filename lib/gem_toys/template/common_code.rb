164a2a8a3961d80b98037e71c55beee60decce54
Alexander Popov <alex.wayfer@gmail.com>
2020-11-11 16:33:23 +0300

    Use `Memery` instead of `@foo ||=`

--- lib/gem_toys/template/common_code.rb
+++ lib/gem_toys/template/common_code.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
+require 'memery'
+
 module GemToys
 	## Template with gem tools, should be expanded in toys file
 	class Template
 		## Module for common code between nested tools
 		module CommonCode
+			include Memery
+
 			private
 
-			def project_name
-				@project_name ||= File.basename context_directory
+			memoize def project_name
+				File.basename context_directory
 			end
 
-			def version_file_path
-				@version_file_path ||=
-					@template.version_file_path ||
+			memoize def version_file_path
+				@template.version_file_path ||
 					begin
 						project_path = project_name.tr '-', '/'
 						File.join context_directory, 'lib', project_path, 'version.rb'
@@ -24,39 +27,37 @@ module GemToys
 				File.read version_file_path
 			end
 
-			def current_version
-				@current_version ||= version_file_content.match(/VERSION = '(.+)'/)[1]
+			memoize def current_version
+				version_file_content.match(/VERSION = '(.+)'/)[1]
 			end
 
-			def changelog_file_path
-				@changelog_file_path ||= File.join context_directory, 'CHANGELOG.md'
+			memoize def changelog_file_path
+				File.join context_directory, 'CHANGELOG.md'
 			end
 
-			def gem_file_name
-				@gem_file_name ||= "#{project_name}-#{current_version}.gem"
+			memoize def gem_file_name
+				"#{project_name}-#{current_version}.gem"
 			end
 
-			def pkg_directory
-				@pkg_directory ||= "#{context_directory}/pkg"
+			memoize def pkg_directory
+				"#{context_directory}/pkg"
 			end
 
 			def current_gem_file
 				"#{pkg_directory}/#{gem_file_name}"
 			end
 
-			def rubygems_connection
-				@rubygems_connection ||= begin
-					require 'faraday'
-					require 'faraday_middleware'
+			memoize def rubygems_connection
+				require 'faraday'
+				require 'faraday_middleware'
 
-					Faraday.new 'https://rubygems.org/api/v1' do |conn|
-						conn.response :json, parser_options: { symbolize_names: true }
-					end
+				Faraday.new 'https://rubygems.org/api/v1' do |conn|
+					conn.response :json, parser_options: { symbolize_names: true }
 				end
 			end
 
-			def rubygems_versions
-				@rubygems_versions ||= rubygems_connection.get("versions/#{project_name}.json").body
+			memoize def rubygems_versions
+				rubygems_connection.get("versions/#{project_name}.json").body
 			end
 
 			def current_version_hash
