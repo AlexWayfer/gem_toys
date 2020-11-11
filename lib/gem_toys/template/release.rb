52509ffbf4e168424b9b5e524dfc3980b5c8f950
Alexander Popov <alex.wayfer@gmail.com>
2020-11-11 17:40:35 +0300

    Add an ability to release new major, minor or patch version
    Just specify keyword instead of number and it'll increase correctly.

--- lib/gem_toys/template/release.rb
+++ lib/gem_toys/template/release.rb
@@ -24,6 +24,8 @@ module GemToys
 
 						@today = Date.today.to_s
 
+						handle_new_version
+
 						update_version_file
 
 						## Update CHANGELOG
@@ -35,13 +37,13 @@ module GemToys
 						wait_for_manual_check
 
 						## Checkout to a new git branch, required for protected `master` with CI
-						# sh "git switch -c v#{new_version}"
+						# sh "git switch -c v#{@new_version}"
 
 						commit_changes
 
 						## Tag commit
 						puts 'Tagging the commit...'
-						sh "git tag -a v#{new_version} -m 'Version #{new_version}'"
+						sh "git tag -a v#{@new_version} -m 'Version #{@new_version}'"
 
 						## Push commit
 						puts 'Pushing commit...'
@@ -57,12 +59,42 @@ module GemToys
 
 					private
 
+					self::VERSION_TYPES = %w[
+						major
+						minor
+						patch
+					].freeze
+
+					def handle_new_version
+						## https://github.com/dazuma/toys/issues/103
+						@new_version =
+							if (version_index = self.class::VERSION_TYPES.index(new_version))
+								current_version_parts =
+									current_version.split('.')[0..(self.class::VERSION_TYPES.count - 1)]
+								handle_keyword_in_new_version current_version_parts, version_index
+							else
+								new_version
+							end
+					end
+
+					def handle_keyword_in_new_version(current_version_parts, version_index)
+						current_version_parts.map.with_index do |version_part, index|
+							if index < version_index
+								version_part
+							elsif index == version_index
+								Integer(version_part) + 1
+							else
+								0
+							end
+						end.join('.')
+					end
+
 					def update_version_file
 						puts 'Updating version file...'
 
 						File.write(
 							version_file_path,
-							version_file_content.sub(/(VERSION = )'.+'/, "\\1'#{new_version}'")
+							version_file_content.sub(/(VERSION = )'.+'/, "\\1'#{@new_version}'")
 						)
 					end
 
@@ -71,7 +103,7 @@ module GemToys
 
 						@changelog_lines = File.readlines(changelog_file_path)
 
-						existing_line = @changelog_lines.find { |line| line.start_with? "## #{new_version} " }
+						existing_line = @changelog_lines.find { |line| line.start_with? "## #{@new_version} " }
 
 						if existing_line
 							return if (existing_date = existing_line.match(/\((.*)\)/)[1]) == @today
@@ -86,7 +118,7 @@ module GemToys
 						unreleased_title = @template.unreleased_title
 						@changelog_lines.insert(
 							@changelog_lines.index("#{unreleased_title}\n") + 2,
-							'#' * unreleased_title.scan(/^#+/).first.size + " #{new_version} (#{@today})\n\n"
+							'#' * unreleased_title.scan(/^#+/).first.size + " #{@new_version} (#{@today})\n\n"
 						).join
 					end
 
@@ -95,7 +127,7 @@ module GemToys
 
 						sh "git add #{version_file_path} #{changelog_file_path}"
 
-						sh "git commit -m 'Update version to #{new_version}'"
+						sh "git commit -m 'Update version to #{@new_version}'"
 					end
 
 					def wait_for_manual_check
